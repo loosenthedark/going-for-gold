f601b981aa8f7c08ddb91c8a49395558883ff64f
Paul Harrington <hello@loosenthedark.tech>
2021-09-04 13:26:52 +0100

    Import Landing component and implement core React Router Switch functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+// STEP 10 = Import the Route and Switch components from React Router...
+import { Route, Switch } from "react-router-dom";
+
+// STEP 11 = Import app component(s)...
 import Landing from "./Landing";
 
+// STEP 12 = Implement Switch functionality with conditional component rendering (including an error-handler) inside the app's main return statement...
 function App() {
-  return <h1>Going for Gold!</h1>;
+  return (
+    <>
+      <h1>Going for Gold!</h1>
+      <Switch>
+        <Route path='/' exact>
+          <Landing />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
