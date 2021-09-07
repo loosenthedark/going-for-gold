7b8c77b2a915c9df7393f0790f3df837b279dc56
Paul Harrington <hello@loosenthedark.tech>
2021-09-07 15:08:54 +0100

    Add landing page links and CTA button to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Nav from "../components/NavMenu";
 
 const About = () => {
@@ -8,14 +9,16 @@ const About = () => {
         <Nav />
         <div className='overlay-about flex-centre trbl-0'></div>
         <div className='overlay-about-text flex-centre trbl-0'>
-          <h1>Going for Gold!</h1>
+          <h1>
+            <Link to='/'>Going for Gold!</Link>
+          </h1>
           <p>
             Unfolding in the wake of a global pandemic, the Tokyo 2020 Olympics
             were a Games like no other.
           </p>
           <p>
-            Now <span>Going for Gold</span> has compiled an Olympic medal count
-            like no other, bringing users a uniquely fair and balanced
+            Now <Link to='/'>Going for Gold</Link> has compiled an Olympic medal
+            count like no other, bringing users a uniquely fair and balanced
             statistical perspective.
           </p>
           <p>
@@ -24,6 +27,9 @@ const About = () => {
             challenge people's attitudes and implicit biases, while at the same
             time championing the underdog and having some fun!
           </p>
+          <Link to='/medallists' className='btn btn-landing bg-gold'>
+            Medallists
+          </Link>
         </div>
       </div>
     </>
