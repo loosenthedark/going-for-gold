49bb60387bb6be403f1fdca166ccb418f46b45ea
Paul Harrington <hello@loosenthedark.tech>
2021-09-07 10:18:43 +0100

    Style hamburger icon relative to search component with z-index and animation

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const NavMenu = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <svg className='hamburger Header__toggle-svg' viewBox='0 0 48 48'>
+        <g
+          stroke='hsl(229, 48%, 24%)'
+          strokeWidth='3'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path id='top-line' d='M12,16 L36,16 Z'></path>
+          <path id='middle-line' d='M12,24 L36,24 Z'></path>
+          <path id='bottom-line' d='M12,32 L36,32 Z'></path>
+        </g>
+      </svg>
+    </nav>
+  );
 };
 
 export default NavMenu;
