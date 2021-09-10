ab1dbcab6a1c7f2a4855d61971d4b5bb8aafaec5
Paul Harrington <hello@loosenthedark.tech>
2021-09-10 10:40:41 +0100

    UX: Provide error handling feedback to user when they enter a search filter term that returns no countries from the array

--- README.md
+++ README.md
@@ -42,6 +42,8 @@ The [JPG to PNG online editing tool](https://jpg2png.com/) was used to convert a
 
 ['Ternary operator on style with React Js Es 6'](https://stackoverflow.com/questions/41092677/ternary-operator-on-style-with-react-js-es-6) (Stack Overflow)
 
+i followed the advice of [this answer](https://stackoverflow.com/a/22480938/12176426) to [this Stack Overflow query](https://stackoverflow.com/questions/487073/how-to-check-if-element-is-visible-after-scrolling) in order to dynamically add a CSS animation class to country flags when they are (vertically) scrolled into view
+
 [Summary of the situation surrounding Russian (ROC) athletes' participation at the Tokyo Games](https://en.wikipedia.org/wiki/Russian_Olympic_Committee_athletes_at_the_2020_Summer_Olympics) (Wikipedia)
 
 I had to intercept the data being returned from the REST Countries API for Russia: changing the name from ['Russian Federation'](https://restcountries.eu/rest/v2/name/russia) to 'ROC' and the flag image's `src` value to [a URL found on the official Olympics.com website](https://olympics.com/tokyo-2020/olympic-games/resCOMMON/img/flags/ROC.png).
--- src/components/Medallists.js
+++ src/components/Medallists.js
@@ -1,18 +1,26 @@
-import React, { useState, useRef } from "react";
-import { useScrollPosition } from "@n8tb1t/use-scroll-position";
+import React from "react";
 import { FaAward } from "react-icons/fa";
+import { ImWarning } from "react-icons/im";
 import { useGlobalContext } from "../context";
 import flagROC from "../static/images/logos/flag-roc.png";
+import flagOlympic from "../static/images/logos/flag-olympic.png";
 import Loading from "./Loading";
 import Toggle from "./ToggleSwitch";
 import Search from "./SearchInput";
 import NavToggler from "./NavToggler";
 
 const Medallists = () => {
-  // STEP 26 = Invoke custom useGlobalContext hook and access movies array as well as isLoading state value...
+  // Invoke custom useGlobalContext hook and access movies array as well as isLoading state value...
   const { medalCountries, isLoading, toggledIcon, searchQuery } =
     useGlobalContext();
 
+  const filteredMedalCountries = medalCountries.filter((country) => {
+    return searchQuery === "" || searchQuery.length < 3
+      ? country
+      : country.name.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
+  // Utility function obtained from this Stack Overflow thread: https://stackoverflow.com/questions/487073/how-to-check-if-element-is-visible-after-scrolling/22480938#22480938
   const isScrolledIntoView = (el) => {
     const rect = el.getBoundingClientRect();
     const elemTop = rect.top;
@@ -36,13 +44,52 @@ const Medallists = () => {
     }, 3000);
   };
 
-  // STEP 27 = Set up multiple conditional returns:
-  // Condition 1: loading condition...
+  // Set up multiple conditional returns:
+  // Condition 1: No medallists found based on user search criteria...
+  if (
+    document.readyState === "complete" &&
+    filteredMedalCountries.length === 0
+  ) {
+    return (
+      <>
+        <Toggle />
+        <Search />
+        <NavToggler />
+        <section>
+          <article className='flex-centre-j article-error'>
+            <div
+              className={`container-flag container-flag-Americas flex-centre`}
+            >
+              <div className='wrapper-flag'>
+                <img className='flag' src={flagOlympic} alt='' />{" "}
+                <div className='flag-olympic trbl-0'> </div>{" "}
+              </div>{" "}
+            </div>{" "}
+            <div>
+              <h3>No medallists found!</h3>{" "}
+              <div>
+                <p>Please try another search...</p>
+                <ul>
+                  <li>
+                    <div className='wrapper-rank'>
+                      <ImWarning className='icon-warning' />
+                    </div>{" "}
+                  </li>{" "}
+                </ul>{" "}
+              </div>{" "}
+            </div>{" "}
+          </article>
+        </section>{" "}
+      </>
+    );
+  }
+
+  // Condition 2: loading condition...
   if (isLoading) {
     return <Loading />;
   }
   // Condition 2 (default): "true return" condition...
-  // STEP 28 = Iterate over the medalCountries array, returning a standalone article element for each country...
+  // Iterate over the medalCountries array, returning a standalone article element for each country...
   return (
     <>
       <Toggle />
@@ -50,138 +97,123 @@ const Medallists = () => {
       <NavToggler />
       <section>
         {" "}
-        {medalCountries
-          .filter((country) => {
-            return searchQuery === "" || searchQuery.length < 3
-              ? country
-              : country.name.toLowerCase().includes(searchQuery.toLowerCase());
-          })
-          .map((country, index) => {
-            // STEP 29 = Destructure all relevant props from country object in fn arg, allocating more appropriate/simplified aliases where necessary...
-            const {
-              alpha3Code: id,
-              name,
-              flag,
-              population,
-              region,
-              totalMedals,
-              goldMedals,
-              goldsPerMillion,
-              medalsPerMillion,
-            } = country;
+        {filteredMedalCountries.map((country, index) => {
+          // Destructure all relevant props from country object in fn arg, allocating more appropriate/simplified aliases where necessary...
+          const {
+            alpha3Code: id,
+            name,
+            flag,
+            population,
+            region,
+            totalMedals,
+            goldMedals,
+            goldsPerMillion,
+            medalsPerMillion,
+          } = country;
 
-            // STEP 30 = Link functionality here needs to be dynamic, so template literal syntax should be used...
-            return (
-              // <Link key={country.id} className='' to={`/country/${id}`}>
-              <article className='flex-centre-j' key={id}>
-                <div
-                  className={`container-flag container-flag-${region} flex-centre`}
-                >
-                  <div className='wrapper-flag'>
-                    <img
-                      className='flag'
-                      src={name === "Russian Federation" ? { flagROC } : flag}
-                      alt={`${
-                        name === "Russian Federation"
-                          ? "ROC"
-                          : name === "United States of America"
-                          ? "USA"
-                          : name === "Syrian Arab Republic"
-                          ? "Syria"
-                          : name === "Moldova (Republic of)"
-                          ? "Moldova"
-                          : name === "Iran (Islamic Republic of)"
-                          ? "Iran"
-                          : name ===
-                            "Macedonia (the former Yugoslav Republic of)"
-                          ? "North Macedonia"
-                          : name === "Venezuela (Bolivarian Republic of)"
-                          ? "Venezuela"
-                          : name === "Republic of Kosovo"
-                          ? "Kosovo"
-                          : name ===
-                            "United Kingdom of Great Britain and Northern Ireland"
-                          ? "Great Britain"
-                          : name
-                      } flag`}
-                    />{" "}
-                    <div className='flag-olympic trbl-0'> </div>{" "}
-                  </div>{" "}
+          // Link functionality here needs to be dynamic, so template literal syntax should be used...
+          return (
+            // <Link key={country.id} className='' to={`/country/${id}`}>
+            <article className='flex-centre-j' key={id}>
+              <div
+                className={`container-flag container-flag-${region} flex-centre`}
+              >
+                <div className='wrapper-flag'>
+                  <img
+                    className='flag'
+                    src={name === "Russian Federation" ? { flagROC } : flag}
+                    alt={`${
+                      name === "Russian Federation"
+                        ? "ROC"
+                        : name === "United States of America"
+                        ? "USA"
+                        : name === "Syrian Arab Republic"
+                        ? "Syria"
+                        : name === "Moldova (Republic of)"
+                        ? "Moldova"
+                        : name === "Iran (Islamic Republic of)"
+                        ? "Iran"
+                        : name === "Macedonia (the former Yugoslav Republic of)"
+                        ? "North Macedonia"
+                        : name === "Venezuela (Bolivarian Republic of)"
+                        ? "Venezuela"
+                        : name === "Republic of Kosovo"
+                        ? "Kosovo"
+                        : name ===
+                          "United Kingdom of Great Britain and Northern Ireland"
+                        ? "Great Britain"
+                        : name
+                    } flag`}
+                  />{" "}
+                  <div className='flag-olympic trbl-0'> </div>{" "}
                 </div>{" "}
-                <div>
-                  <h2>
-                    {" "}
-                    {name === "Russian Federation"
-                      ? "ROC"
-                      : name === "United States of America"
-                      ? "USA"
-                      : name === "Syrian Arab Republic"
-                      ? "Syria"
-                      : name === "Moldova (Republic of)"
-                      ? "Moldova"
-                      : name === "Iran (Islamic Republic of)"
-                      ? "Iran"
-                      : name === "Macedonia (the former Yugoslav Republic of)"
-                      ? "North Macedonia"
-                      : name === "Venezuela (Bolivarian Republic of)"
-                      ? "Venezuela"
-                      : name === "Republic of Kosovo"
-                      ? "Kosovo"
-                      : name ===
-                        "United Kingdom of Great Britain and Northern Ireland"
-                      ? "Great Britain"
-                      : name}{" "}
-                  </h2>{" "}
-                  <ul>
-                    <li>
-                      Population:{" "}
-                      <span>
-                        {" "}
-                        {population
-                          .toString()
-                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-                      </span>{" "}
-                    </li>{" "}
-                    <li
-                      className={
-                        !toggledIcon ? "underline underline-gold" : null
-                      }
-                    >
-                      Gold medals: <span> {goldMedals} </span>{" "}
-                    </li>{" "}
-                    <li
-                      className={
-                        toggledIcon ? "underline underline-gold" : null
-                      }
-                    >
-                      Total medals: <span> {totalMedals} </span>{" "}
-                    </li>{" "}
-                    <li
-                      className={
-                        !toggledIcon ? "underline underline-gold" : null
-                      }
-                    >
-                      Golds per million: <span> {goldsPerMillion} </span>{" "}
-                    </li>{" "}
-                    <li
-                      className={
-                        toggledIcon ? "underline underline-gold" : null
-                      }
-                    >
-                      Total per million: <span> {medalsPerMillion} </span>{" "}
-                    </li>{" "}
-                    <li>
-                      <div className='wrapper-rank'>
-                        <FaAward className='icon-award-golds' />{" "}
-                        <span> {index + 1} </span>{" "}
-                      </div>{" "}
-                    </li>{" "}
-                  </ul>{" "}
-                </div>{" "}
-              </article>
-              // </Link>
-            );
-          })}{" "}
+              </div>{" "}
+              <div>
+                <h2>
+                  {" "}
+                  {name === "Russian Federation"
+                    ? "ROC"
+                    : name === "United States of America"
+                    ? "USA"
+                    : name === "Syrian Arab Republic"
+                    ? "Syria"
+                    : name === "Moldova (Republic of)"
+                    ? "Moldova"
+                    : name === "Iran (Islamic Republic of)"
+                    ? "Iran"
+                    : name === "Macedonia (the former Yugoslav Republic of)"
+                    ? "North Macedonia"
+                    : name === "Venezuela (Bolivarian Republic of)"
+                    ? "Venezuela"
+                    : name === "Republic of Kosovo"
+                    ? "Kosovo"
+                    : name ===
+                      "United Kingdom of Great Britain and Northern Ireland"
+                    ? "Great Britain"
+                    : name}{" "}
+                </h2>{" "}
+                <ul>
+                  <li>
+                    Population:{" "}
+                    <span>
+                      {" "}
+                      {population
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                    </span>{" "}
+                  </li>{" "}
+                  <li
+                    className={!toggledIcon ? "underline underline-gold" : null}
+                  >
+                    Gold medals: <span> {goldMedals} </span>{" "}
+                  </li>{" "}
+                  <li
+                    className={toggledIcon ? "underline underline-gold" : null}
+                  >
+                    Total medals: <span> {totalMedals} </span>{" "}
+                  </li>{" "}
+                  <li
+                    className={!toggledIcon ? "underline underline-gold" : null}
+                  >
+                    Golds per million: <span> {goldsPerMillion} </span>{" "}
+                  </li>{" "}
+                  <li
+                    className={toggledIcon ? "underline underline-gold" : null}
+                  >
+                    Total per million: <span> {medalsPerMillion} </span>{" "}
+                  </li>{" "}
+                  <li>
+                    <div className='wrapper-rank'>
+                      <FaAward className='icon-award-golds' />{" "}
+                      <span> {index + 1} </span>{" "}
+                    </div>{" "}
+                  </li>{" "}
+                </ul>{" "}
+              </div>{" "}
+            </article>
+            // </Link>
+          );
+        })}{" "}
       </section>{" "}
     </>
   );
